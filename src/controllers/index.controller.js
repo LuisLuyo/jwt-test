30bef12ad532183e76e66ac1373779fabd3df89e
PC-LuisLuyo <luis.luyohernandez@gmail.com>
2020-03-02 08:33:34 -0500

    Genera token y valida usuario/clave

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const pool = new Pool({
     user: 'adminfrances1720',
-    host: '64.227.1.145',
+    host: '68.183.103.192',
     password: 'Frances1720@2020',
     database: 'francesTest',
     port: '5432'
@@ -20,19 +20,39 @@ const getToken = async (req,res) => {
     //const usuarioParameter = String(req.params.usuario);
     //const claveParameter = String(req.params.clave);
     const { usuario, clave } = req.body;
-    const usuariores = await pool.query('SELECT usuario, clave, estado FROM SIGV_SEGURIDAD.USUARIO WHERE USUARIO = $1 AND CLAVE = $2;', [usuario,clave]);
+    const usuariores = await pool.query('SELECT usuario, estado FROM SIGV_SEGURIDAD.USUARIO WHERE USUARIO = $1 AND CLAVE = $2;', [usuario,clave]);
     //console.log(usuariores);
     //debo obtener el usuario y contraseña insertado desde el body
     //aqui debo acceder a la base de datos a Validar el usuario y la contraseña ingresada
     //según sea la respuesta debo validar si se genera o no el token
-    const token = jwt.sign({usuariores}, 'my_secret_key', { expiresIn: '30s' });
-    res.status(200).json({
-        message: 'Token generated successfully!',
-        body: {
-            usuario: usuariores.rows,
-            jwt : token
-        }
-    });
+    const rows = usuariores.rows;
+    if(!isEmptyObject(usuariores.rows)){
+        console.log(usuariores.rows);
+        const token = jwt.sign({usuariores}, 'my_secret_key', { expiresIn: '30s' });
+        res.status(200).json({
+            message: 'Token generated successfully!',
+            body: {
+                usuario: usuariores.rows,
+                jwt : token
+            }
+        });
+    }
+    else {
+        res.status(200).json({
+            message: 'Error en usuario/contraseña, volver a intentar!',
+            body: {
+            }
+        });
+    }
+}
+
+function isEmptyObject(obj) {
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 const getUsuario = async (req, res) => {
