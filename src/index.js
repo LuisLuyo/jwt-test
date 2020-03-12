aaac8b79eff00bbd312084d85163f2650ab76aa3
“PC-Stefanini” <luis.luyohernandez@gmail.com>
2020-03-11 22:25:04 -0500

    Prueba de subida a Docker Hub Automatizado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 
+/* Esto es un comentario */
 async function init() {
     await app.listen(process.env['SERVER_PORT']);
     console.log('Server on port ' + process.env['SERVER_PORT']);
