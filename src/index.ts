cf696b925db236bb36e189713bca29b6d5cf8093
“PC-Stefanini” <luis.luyohernandez@gmail.com>
2020-03-28 14:12:43 -0500

    Se añade construcción TSC al Docker File

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import app from "./app";
 /* Esto es un comentario commit de docker */
 async function init() {
     await app.listen(process.env['SERVER_PORT']);
-    console.log('Server on port *********', process.env['SERVER_PORT']);
-    console.log('JWT SIGV connect to*****', process.env['AMBIENTE']);
+    console.log('Server on port ======', process.env['SERVER_PORT']);
+    console.log('JWT SIGV connect to==', process.env['AMBIENTE']);
 }
 init();
