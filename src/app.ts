d7ae992ee533ad7286ccb9cad6e989b11d66fd58
Luis Luyo <jean.luyo@bbva.com>
2021-05-18 20:18:56 -0500

    Configurando puerto seguro.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
+import fs from 'fs';
 import express, { Application } from 'express';
 import './utils/config';
 import indexRoutes from './routes/index';
 import cors from 'cors';
-
+import http from 'https';
+import https from 'https';
 const app: Application = express();
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/frances1720.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/frances1720.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/frances1720.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 // middlewares
 app.use(express.json());
@@ -14,4 +27,5 @@ app.use(cors({
 // Routes
 app.use(indexRoutes);
 
-export default app;
+export const httpServer = http.createServer(app);
+export const httpsServer = https.createServer(credentials, app);
